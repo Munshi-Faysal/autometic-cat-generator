6086bc011986bd8fd3e4e03c792ab0eff17996d9
Munshi-Faysal <61263251+Munshi-Faysal@users.noreply.github.com>
2021-06-12 11:18:22 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function genCat(){
+    let image = document.createElement('img');
+    let div = document.getElementById('flex-cat-gen');
+    image.src = "http://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+    div.appendChild(image);
+}
